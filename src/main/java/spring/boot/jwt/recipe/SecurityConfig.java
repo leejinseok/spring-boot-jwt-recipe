d8d6b34894edfca133d911e1afc0757805be7773
leejinseok <sonaky47@gmail.com>
2022-03-19 19:18:45 +0900

    스프링, 시큐리티, 어플리케이션 구조

---
+++ src/main/java/spring/boot/jwt/recipe/SecurityConfig.java
@@ -0,0 +1,11 @@
+package spring.boot.jwt.recipe;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
